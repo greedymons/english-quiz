4244bbe5db1212e9fb4eb4c703995dce90978151
romizaki <romizaki@trustmedis.co.id>
2021-08-28 14:58:46 +0700

    wiring quiz

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,19 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-function Card(params) {
+function Card(props) {
+  const [options] = useState(props.data.options)
+  console.log(options,"???????????");
   return (
     <>
-      <div class="card text-center">
-        <div class="card-header">
-          Featured
+      <div className="card container" style={{width:"65%"}}>
+        <div className="card-header text-end">
+          Question Number {props.data.id}
         </div>
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <Link to={"/"} class="btn btn-primary">Go somewhere</Link>
+      <div className="card-body">
+        {
+          props.data.story ? <p className="card-text">{props.data.story}</p> : ""
+        }
+
+        <h5 className="card-title">{props.data.question}</h5>
+   
+      <div className="row">
+        <div className="col-sm-10">
+          <div className="form-check">
+            <input className="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" />
+            <label className="form-check-label" for="gridRadios1">
+              {options[0]}
+            </label>
+          </div>
+          
+          <div className="form-check">
+            <input className="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2" />
+            <label className="form-check-label" for="gridRadios2">
+              {options[1]}
+            </label>
+          </div>
+
+          <div className="form-check">
+            <input className="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" />
+            <label className="form-check-label" for="gridRadios3">
+              {options[2]}
+            </label>
+          </div>
+
+          <div className="form-check">
+            <input className="form-check-input" type="radio" name="gridRadios" id="gridRadios4" value="option4" />
+            <label className="form-check-label" for="gridRadios4">
+              {options[3]}
+            </label>
+          </div>
+          
+        </div>
       </div>
-        <div class="card-footer text-muted">
+
+      </div>
+        <Link to={"/"} className="btn btn-primary btn-sm">Go somewhere</Link>
+        <div className="card-footer text-muted text-end">
           2 days ago
         </div>
       </div>
--- src/pages/quizPage.jsx
+++ src/pages/quizPage.jsx
@@ -3,18 +3,18 @@ import Card from '../components/card'
 import { useSelector, useDispatch } from 'react-redux'
 
 
-function QuizPage(params) {
+function QuizPage() {
   const dispatch = useDispatch()
   const { questions, loading, error } = useSelector(state => state)
 
+  const num = JSON.parse(localStorage.getItem('quiz')).currentQuestion
+  console.log(num,"<<<<<<");
   return (
     <>
       <h1>Helooo world from quiz page</h1>
-      {
-        JSON.stringify(questions)
-      }
-      <h2>wkwkwk</h2>
-      <Card />
+      <div className="container">
+        <Card data={questions[num-1]} />
+      </div>
     </>
   )
 }
